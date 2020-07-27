8e2b25742bcd3b854c23b4e7e86447cf5ccae561
benjamim015 <benjamimq@hotmail.com>
2020-07-27 17:45:53 -0400

    Added .env

--- cloudantDBFunctions.js
+++ cloudantDBFunctions.js
@@ -2,14 +2,6 @@
 
 // const Cloudant = require("@cloudant/cloudant");
 
-// var cloudant = new Cloudant({
-//   url:
-//     "https://ca1f752f-4a4f-438d-8246-7840da8ffd1c-bluemix.cloudantnosqldb.appdomain.cloud",
-//   plugins: {
-//     iamauth: { iamApiKey: "x26hK6mbe9VbhZy346ko1EAgrrWnL_ML2KRYXpeBbozu" },
-//   },
-// });
-
 // var dbname = "crud";
 // var db = null;
 // var doc = null;
--- ibmCloudant.js
+++ ibmCloudant.js
@@ -1,10 +1,9 @@
 const Cloudant = require("@cloudant/cloudant");
 
 const cloudant = new Cloudant({
-  url:
-    "https://ca1f752f-4a4f-438d-8246-7840da8ffd1c-bluemix.cloudantnosqldb.appdomain.cloud",
+  url: process.env.URL,
   plugins: {
-    iamauth: { iamApiKey: "x26hK6mbe9VbhZy346ko1EAgrrWnL_ML2KRYXpeBbozu" },
+    iamauth: { iamApiKey: process.env.IAM_API_KEY },
   },
 });
 
